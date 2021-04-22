21a3d0721fa1eaa0936c8cba53c984b3232ec109
Yordi <yraulbr@hotmail.com>
2021-04-21 22:42:33 -0600

    [add]Actualización de valor de variables y conteo de referencias.

---
+++ cmake-build-debug/CMakeFiles/Test.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/Communication/HTTPServer.cpp" "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/cmake-build-debug/CMakeFiles/Test.dir/Communication/HTTPServer.cpp.o"
+  "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/Communication/Socket.cpp" "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/cmake-build-debug/CMakeFiles/Test.dir/Communication/Socket.cpp.o"
+  "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/Scope/Scope.cpp" "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/cmake-build-debug/CMakeFiles/Test.dir/Scope/Scope.cpp.o"
+  "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/VariableManager.cpp" "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/cmake-build-debug/CMakeFiles/Test.dir/VariableManager.cpp.o"
+  "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/pruebaskk.cpp" "/home/yordi/Documentos/GitKraken/Datos-II-Proyecto-1-Servidor/cmake-build-debug/CMakeFiles/Test.dir/pruebaskk.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/Communication/HTTPServer.cpp.o"
+  "CMakeFiles/Test.dir/Communication/Socket.cpp.o"
+  "CMakeFiles/Test.dir/Scope/Scope.cpp.o"
+  "CMakeFiles/Test.dir/VariableManager.cpp.o"
+  "CMakeFiles/Test.dir/pruebaskk.cpp.o"
+  "Test"
+  "Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
